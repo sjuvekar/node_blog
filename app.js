44b12c07e3c96692bf5b183a70f1b187a39b2033
Sudeep Juvekar <sjuvekar@gmail.com>
2016-01-26 13:12:17 -0500

    authentication
    Change-Id: I8ffb18b2726b3c84a98f63299578b08338c87f0f

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 var express = require('express');
+var session = require('express-session')
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var login = require('./routes/login');
+var routes = require('./routes/');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,10 +20,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({ secret: 'JenkinsUbuntuGerrit' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/login', login);
+app.get('/', function (req, res) { routes.render("index", req, res) });
+app.get('/:id?', function(req, res) { routers.render("index", req, res)})
+app.use('/login', function(req, res) { routes.login(req, res)});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
