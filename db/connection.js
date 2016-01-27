df9dc258942dbd301fb17111fa7532bd501464d6
Sudeep Juvekar <sjuvekar@tower-research.com>
2016-01-27 15:38:52 -0500

    debug
    Change-Id: I70cfb0272a948f882a2e57ef98052ca0b4eff2b9

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 
-var uristring = "mongodb://" + process.env.MONGODB_USER + ":" + process.env.MONGODB_PASSWORD + "@ds051575.mongolab.com:51575/heroku_fjlz6w1r";
+var uristring =  process.env.MONGOLAB_URI ||
+  				 process.env.MONGOHQ_URL ||
+  				"mongodb://" + process.env.MONGODB_USER + ":" + process.env.MONGODB_PASSWORD + "@ds051575.mongolab.com:51575/heroku_fjlz6w1r";
 
 module.exports.mongoInit = function() {
     mongoose.connect(uristring, function (err, res) {
