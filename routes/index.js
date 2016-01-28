fd3a90306d5a42183de1bc776c2c937bf786ec8f
Sudeep Juvekar <sjuvekar@gmail.com>
2016-01-28 08:53:24 -0500

    post details
    Change-Id: If845c2f8199b6a37cbfbc0c7f829ecdeb5c968a7

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,31 @@
 var Post = require('../db/post')
 
 module.exports.render = function(dest, req, res) {
-  var my_id = 1;
-  if (req.params.id)
-  {
-    my_id = req.params.id;
-  }
+	var my_id = 1;
+	if (req.params.id) {
+		my_id = req.params.id;
+	}
 	Post.find({}, null, {sort: {created_at: -1}})
-      .skip((my_id-1) * 10)
-      .limit(10)
-      .exec(function(err, result) {
-		    if (!err) {
-			    res.render(dest, {
-				    blog_entries: result,
-				    num_pages: 6,
-			    });
-		    }
-	    });
+	.skip((my_id-1) * 10)
+	.limit(10)
+	.exec(function(err, result) {
+		if (!err) {
+			res.render(dest, {
+				blog_entries: result,
+				num_pages: 6,
+			});
+		}
+	});
+ };
+
+module.exports.render_blog = function(dest, req, res) {
+  	if (req.params.id) {
+		Post.findById(req.params.id, function(err, result) {
+			if (!err) {
+				res.render(dest, {
+					blog_entry: result,
+				});
+			}
+		});
+	}
 };
