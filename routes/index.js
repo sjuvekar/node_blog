4106bfbf00e274c7e5722febc5468f91789925e4
Sudeep Juvekar <sjuvekar@gmail.com>
2016-01-28 09:13:54 -0500

    Get correct number of pages
    Change-Id: I7bc184b5fdf49d18b85fed1a56d3fdd272078b3d

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,30 @@
 var Post = require('../db/post')
 
 module.exports.render = function(dest, req, res) {
-	var my_id = 1;
-	if (req.params.id) {
-		my_id = req.params.id;
-	}
-	Post.find({}, null, {sort: {created_at: -1}})
-	.skip((my_id-1) * 10)
-	.limit(10)
-	.exec(function(err, result) {
+	// First send number of documents in database
+	Post.count(function(err, count) {
 		if (!err) {
-			res.render(dest, {
-				blog_entries: result,
-				num_pages: 6,
+			// Get current page id from request
+			var my_id = 1;
+			if (req.params.id) {
+				my_id = req.params.id;
+			}
+
+			// Render the page using documents
+			Post.find({}, null, {sort: {created_at: -1}})
+			.skip((my_id-1) * 10)
+			.limit(10)
+			.exec(function(err, result) {
+				if (!err) {
+					res.render(dest, {
+						blog_entries: result,
+						num_pages: Math.ceil(count / 10),
+					});
+				}
 			});
 		}
 	});
- };
+};
 
 module.exports.render_blog = function(dest, req, res) {
   	if (req.params.id) {
